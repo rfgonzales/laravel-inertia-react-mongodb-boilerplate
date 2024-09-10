1fe65766d93254608ba396181c3c7ced0a0a06b7
Fritz <rfgonzales12@gmail.com>
2024-09-11 01:29:35 +0800

    seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin_laravel@yopmail.com',
+            'password' => Hash::make('admin_laravel_1'),
+            'email_verified_at' => Carbon::now(),
+        ]);
+    }
+}
