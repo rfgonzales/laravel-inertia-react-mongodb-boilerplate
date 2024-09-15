c67ec1c7d969f3466eb4a6a8ae1c81458d64a201
Fritz <rfgonzales12@gmail.com>
2024-09-15 12:51:45 +0800

    fixing tailwind

--- resources/js/Components/TextInput.tsx
+++ resources/js/Components/TextInput.tsx
@@ -21,7 +21,7 @@ export default forwardRef(function TextInput(
             {...props}
             type={type}
             className={
-                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
+                'tw-border-gray-300 dark:tw-border-gray-700 dark:tw-bg-gray-900 dark:tw-text-gray-300 focus:tw-border-indigo-500 dark:tw-focus:border-indigo-600 focus:tw-ring-indigo-500 dark:tw-focus:ring-indigo-600 tw-rounded-md tw-shadow-sm ' +
                 className
             }
             ref={localRef}
